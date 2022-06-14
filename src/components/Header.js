f5b3ded43f7a713b1c8e3ea7d850013fd1b95b43
stevenoketch6@gmail.com <stevenoketch6@gmail.com>
2022-06-15 01:06:26 +0300

    meme maker using an API > 1st commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return(
+        <header>
+            <img className="header-img" src="https://cdnb.artstation.com/p/assets/images/images/004/456/215/large/wil-hughes-troll-face.jpg?1483926054" alt="websiteLogo" />
+            <h2 className="header-title">Meme Maker</h2>
+            <h4 className="header-project"> React Course - Project 3</h4>
+        </header>
+    )
+}
